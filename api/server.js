e90aa29dd7750dadd0fdbb99d3fa827339a5b741
Robbie Porter <rthomporter@gmail.com>
2020-01-31 18:16:18 -0600

    Basic Backend Structure

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.use(
+  session({
+    name: "",
+    secret: "",
+    cookiue: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: false
+    },
+    httpOnly: false,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+module.exports = server;
