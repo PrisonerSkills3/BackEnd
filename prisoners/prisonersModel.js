c0ea940bb88d0a88dde26a7a0d80989b2071e67f
Robbie Porter <rthomporter@gmail.com>
2020-02-03 13:49:14 -0600

    joined tables in model

--- prisoners/prisonersModel.js
+++ prisoners/prisonersModel.js
@@ -1,19 +1,23 @@
-const db = require('../database/dbConfig');
+const db = require("../database/dbConfig");
 
 module.exports = {
-    find,
-    findById,
-    findByPrison
+  find,
+  findById,
+  findByPrison
+};
 
+function find() {
+  return db("prisoners").select();
 }
-
-function find(){
-     return db('prisoners').select();
+function findById(id) {
+  console.log("id", id);
+  return db("prisoners")
+    .where({ id: id })
+    .select();
 }
-function findById(id){
-   console.log('id',id)
-   return db('prisoners').where({id: id}).select();
-}
-function findByPrison(id){
-   return db('prisoners').where('prison_id','=',id).select();
+function findByPrison(id) {
+  return db("prisoners", "prisons.id")
+    .join("prisons", "prisons.id", "prisoners.prisons_id")
+    .where("prison_id", "=", id)
+    .select();
 }
